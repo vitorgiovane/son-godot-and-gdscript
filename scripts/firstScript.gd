867ba6c71c0aec3c07064f053dde16f5c338ed9c
Vitor <vitorgpalves@gmail.com>
2019-08-27 00:02:30 -0300

    aula 5 - trabalha com arrays

--- scripts/firstScript.gd
+++ scripts/firstScript.gd
@@ -6,6 +6,8 @@ var money = 1200
 var type = "Warrior"
 var isWarrior = true
 var gameName
+var players = ["John Doe", "Maria Doe", "Richard Doe"]
+var animals = [["Dog", "white"], ["Cat", "black"], players]
 
 func _ready():
 	print("Hello, Godot!")
@@ -17,8 +19,13 @@ func _ready():
 	
 	gameName = "Super Godot"
 	print(gameName)
+	
+	print(players[1])
+	
+	print(animals)
+	print(animals[2][2])
 	pass
 
 func _process(delta):
-	print(gameName)
+# 	print(gameName)
 	pass
