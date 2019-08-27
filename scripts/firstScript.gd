cffc2b1f5e44ecdc81e96721e278ac3969bceca0
Vitor <vitorgpalves@gmail.com>
2019-08-26 23:50:44 -0300

    aula 4 - trabalhando com variáveis

--- scripts/firstScript.gd
+++ scripts/firstScript.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+var playerName = "Vitor Giovane"
+var life = 100
+var money = 1200
+var type = "Warrior"
+var isWarrior = true
+var gameName
+
 func _ready():
 	print("Hello, Godot!")
+	print(playerName)
+	print(life)
+	print(money)
+	print(type)
+	print(isWarrior)
+	
+	gameName = "Super Godot"
+	print(gameName)
 	pass
 
 func _process(delta):
-	print("Hello, _process")
+	print(gameName)
 	pass
